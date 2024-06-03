1c3dee2e26ca865dd129587485a4af01d1f13463
AJ-droi <ajiriosiobe@gmail.com>
2024-06-03 11:29:30 +0100

    fixed linting issues in contains.js

--- algorithms/JavaScript/src/recursion/contains.js
+++ algorithms/JavaScript/src/recursion/contains.js
@@ -1,6 +1,8 @@
-//Write a function called `contains` that searches for a value in a nested object. It returns true if the object contains that value.
+// Write a function called `contains` that searches for a value in a nested
+// object.
+// It returns true if the object contains that value.
 
-```javascript
+''`javascript
 var nestedObject = {
     data: {
         info: {
@@ -17,26 +19,28 @@ var nestedObject = {
 
 contains(nestedObject, 44) // true
 contains(nestedObject, "foo") // false
-```
+```;
 
 // Solution
 const contains = (object, checkValue) => {
-    if (object == null || Object.keys(object).length === 0) {
-        return false;
-    }
-
-    for (let [key, value] of Object.entries(object)) {
-        if (key === checkValue || value === checkValue) {
-            return true;
-        }
-        if (typeof value === 'object' && contains(value, checkValue)) {
-            return true;
-        }
-    }
-
+  if (object == null || Object.keys(object).length === 0) {
     return false;
+  }
+
+  for (const [key, value] of Object.entries(object)) {
+    if (key === checkValue || value === checkValue) {
+      return true;
+    }
+    // eslint-disable-next-line no-unused-vars
+    if (typeof value === 'object' && contains(value, checkValue)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
-//Note
-//Time Complexity: 𝑂(𝑛) where n is the total number of key-value pairs in the object.
-//Space Complexity: 𝑂(𝑑) where d is the maximum depth of nested objects.
+// Note
+// Time Complexity: 𝑂(𝑛) where n is the total number of key-value pairs
+// in the object.
+// Space Complexity: 𝑂(𝑑) where d is the maximum depth of nested objects.
